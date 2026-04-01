12a56f7e722292d2efb11caf2d926b5d22e15290
Sylency <77147129+Sylency@users.noreply.github.com>
2026-04-01 17:44:46 +0200

    Fix

--- assets/components.js
+++ assets/components.js
@@ -385,7 +385,7 @@
               <span class="userbox-hello" data-i18n="nav_hello">${helloStr}</span>
               <img src="${avatar}" class="nav-avatar" alt="" crossorigin="anonymous" />
               <strong class="nav-username">${nameStr}</strong>
-              <a href="/api/discord/logout" class="btn-logout" title="Logout"><i class="bi bi-box-arrow-right"></i></a>
+              <a href="/api/auth?action=logout" class="btn-logout" title="Logout"><i class="bi bi-box-arrow-right"></i></a>
             </div>
           `;
 
